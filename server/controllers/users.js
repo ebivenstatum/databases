a92d02f27da0c4639b53c762929804d13becb925
Elizabeth Bivens-Tatum <ebivenstatum@gmail.com>
2022-06-24 17:23:21 -0400

    users seemingly working

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,25 @@
 var models = require('../models');
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept, authorization',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
+};
 
 module.exports = {
   get: function (req, res) {},
-  post: function (req, res) {}
+  post: function (req, res) {
+
+    models.users.create(req.body["username"], (err,data) => {
+      if (err) {
+        res.writeHead(404, defaultCorsHeaders);
+        res.end('Error! ' + JSON.stringify(data));
+      } else {
+        res.writeHead(201, defaultCorsHeaders);
+        res.end('User Added');
+      }
+
+    });
+  }
 };
--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,19 @@
-var db = require('../db');
+var db = require('../db').dbConnection;
+const mysql = require('mysql2');
 
 module.exports = {
   getAll: function () {},
-  create: function () {}
+  create: function (username, callback) {
+
+  let queryString = `INSERT INTO users (username) values ("${username}")`
+
+  db.query(queryString, (err, data) => {
+    if (err) {
+      callback(err, message);
+    } else {
+      callback(null, "db: success");
+    }
+  });
+
+  }
 };
